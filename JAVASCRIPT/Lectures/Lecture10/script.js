fc7b025d9fcba412398a8fb9317c92e44f0b8ae3
Isha713 <ishasaini713@gmail.com>
2026-07-10 06:46:26 +0530

    Add new file

---
+++ JAVASCRIPT/Lectures/Lecture10/script.js
@@ -0,0 +1,39 @@
+//Math Object
+console.log(Math.PI);
+
+console.log(Math.max(10,20,30,40));
+
+console.log(Math.min(10,-25));
+
+console.log(Math.round(1.6));
+
+console.log(Math.floor(41.65));
+
+console.log(Math.ceil(41.21));
+
+console.log(Math.abs(-89));
+
+console.log(Math.random(0,1));
+
+console.log(Math.sqrt(5));
+
+console.log(Math.pow(2,10));
+
+console.log(Math.tan(45));
+
+
+
+
+//Date Object
+let curr = new Date;
+console.log(curr);
+
+let date = new Date("November 15 2005 07:15")
+console.log(date);
+
+let newdate = new Date(1998, 5, 20, 7);
+console.log(newdate);
+
+console.log(newdate.getDay());
+console.log(newdate.getFullYear());
+
